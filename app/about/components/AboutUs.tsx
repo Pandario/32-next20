f38369c4e9fba80d47dd7dbe5a86026c9d236df8
Andrew <dinogalaktika@gmail.com>
2024-03-16 09:40:37 +0100

    carousel added

--- app/about/components/AboutUs.tsx
+++ app/about/components/AboutUs.tsx
@@ -1,21 +1,55 @@
+'use client'
 import Image from "next/image"
+import { useState } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 export default function AboutUs() {
+    const images: string[] = [
+        '/img/about/aboutUs1.jpg',
+        '/img/about/aboutUs2.jpg',
+      ];
+
+      const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+      const goPrevious = () => {
+        setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : images.length - 1));
+      };
+    
+      const goNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex < images.length - 1 ? prevIndex + 1 : 0));
+      };
+
     return (
         <div className="pt-10 m-4">
 
             <div className="flex justify-center ">
             
                 <div className="grid grid-cols-1 lg:grid-cols-2   mx-auto">
-                <div className="items-center order-first lg:order-none justify-center flex flex-col">
-                    <Image
-                        src="/img/aboutUs.jpg"
-                        width={350}
-                        height={350}
-                        alt="Picture of the author"
-                        className="rounded-lg"
-                        priority={true}
-                    />
+                    <div className="items-center order-first lg:order-none justify-center flex relative ">
+
+                        <div className="relative">
+                            <Image
+                                src={images[currentIndex]}
+                                width={350}
+                                height={350}
+                                alt="Picture of the author"
+                                className="rounded-lg "
+                                priority={true}
+                            />
+                            <button
+                                className="absolute top-1/2 left-1 transform -translate-y-1/2 z-10 bg-transparent hover:bg-gray-700 text-gray-800 font-semibold hover:text-white py-2 px-4 border border-gray-800 hover:border-transparent rounded"
+                                onClick={goPrevious}
+                            >
+                                <IoIosArrowBack size="1.5em" />
+                            </button>
+                            <button
+                                className="absolute top-1/2 right-1 transform -translate-y-1/2 z-10 bg-transparent hover:bg-gray-700 text-gray-800 font-semibold hover:text-white py-2 px-4 border border-gray-800 hover:border-transparent rounded"
+                                onClick={goNext}
+                            >
+                                <IoIosArrowForward size="1.5em" />
+                            </button>        
+                        </div>
                     </div>
+
                     <div className="flex flex-col  p-4 my-1 shadow-lg rounded-lg">
 
                         <h1 className="text-3xl font-bold mb-4 text-center">Our Amaizing restaurant {'!'}</h1>
